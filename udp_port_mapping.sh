8389b8668cba00797d60d49d47862ce3dd56c090
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2025-02-28 22:58:11 +0800

    Update udp_port_mapping.sh

--- udp_port_mapping.sh
+++ udp_port_mapping.sh
@@ -6,21 +6,23 @@ if ! command -v sudo &> /dev/null; then
     apt install sudo -y
 fi
 
-# 提示输入连接端口的起始和终止端口
-echo "请输入连接端口（起始）:"
-read start_port
+while true; do
+    echo "请输入连接端口（起始）:"
+    read start_port
 
-echo "请输入连接端口（终止）:"
-read end_port
+    echo "请输入连接端口（终止）:"
+    read end_port
 
-echo "请输入服务端口:"
-read service_port
+    echo "请输入服务端口:"
+    read service_port
 
-# 检查输入格式是否正确
-if [[ ! "$start_port" =~ ^[0-9]+$ ]] || [[ ! "$end_port" =~ ^[0-9]+$ ]] || [[ ! "$service_port" =~ ^[0-9]+$ ]]; then
-    echo "端口格式无效，请重新输入。"
-    exit 1
-fi
+    # 检查输入格式是否正确
+    if [[ ! "$start_port" =~ ^[0-9]+$ ]] || [[ ! "$end_port" =~ ^[0-9]+$ ]] || [[ ! "$service_port" =~ ^[0-9]+$ ]]; then
+        echo "端口格式无效，请重新输入。"
+    else
+        break  # 格式正确时跳出循环
+    fi
+done
 
 # 检查服务端口是否在连接端口范围内
 if [ "$service_port" -ge "$start_port" ] && [ "$service_port" -le "$end_port" ]; then
